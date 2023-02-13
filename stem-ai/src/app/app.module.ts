ddf9dbea9c12a3d2301bdb5e65acdf14f8274c2f
dossanto001 <dossanto001@gannon.edu>
2023-02-13 16:55:52 -0500

    material

--- stem-ai/src/app/app.module.ts
+++ stem-ai/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { InfoComponent } from './info/info.component';
 import { LoginComponent } from './login/login.component';
+
 
 const routes: Routes = [
   { path: 'info', component: InfoComponent },
@@ -21,7 +25,7 @@ imports: [
 ]
 
 @NgModule({
-  imports:      [ BrowserModule, RouterModule.forRoot(routes)],
+  imports:      [ MatSlideToggleModule, MatCheckboxModule, MatCardModule,BrowserModule, RouterModule.forRoot(routes)],
   declarations: [ AppComponent, LoginComponent, InfoComponent ],
   bootstrap:    [ AppComponent ]
 })
