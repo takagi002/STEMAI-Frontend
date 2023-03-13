6029a265de2bb341e4a8f56ec742705a0324f2b4
dossanto001 <dossanto001@gannon.edu>
2023-03-13 13:13:50 -0400

    added professor page

---
+++ stem-ai/src/app/professor-classes/professor-classes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professor-classes',
+  templateUrl: './professor-classes.component.html',
+  styleUrls: ['./professor-classes.component.css']
+})
+export class ProfessorClassesComponent {
+
+}
