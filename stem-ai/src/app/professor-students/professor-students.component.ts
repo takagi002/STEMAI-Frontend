b4e7ac32485940e91690c4e76c57be85b67494fc
Jacob Rose <rose030@gannon.edu>
2023-03-25 01:52:17 +0000

    Merged PR 96: Students for professor on page

--- stem-ai/src/app/professor-students/professor-students.component.ts
+++ stem-ai/src/app/professor-students/professor-students.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProfessorClassesService } from '../services/professor-class-service/professor-classes.service';
+import { SharingService } from '../services/sharing-service/sharing.service';
 
 @Component({
   selector: 'app-professor-students',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./professor-students.component.css']
 })
 export class ProfessorStudentsComponent {
+
+  currentUser: any;
+  userID: any;
+  students: any;
+  currentClass: any;
+
+  constructor(private sharingService: SharingService, private router: Router, private professorClassesService: ProfessorClassesService) {}
+
+  ngOnInit(){
+
+    this.currentUser = this.sharingService.getCurrentUser();
+    this.userID = this.sharingService.getIDNumber();
+    this.currentClass = this.sharingService.getCurrentClass();
+
+    if(Object.keys(this.currentUser).length === 0){
+      this.goToPage("login")
+    }
+
+    this.professorClassesService.getStudentsInClass(this.currentClass).subscribe(res =>{
+      this.students = res;
+    })
+
+  }
+
+  
+  goToPage(pageName:string){
+    this.sharingService.setCurrentUser(this.currentUser);
+    this.router.navigate([`${pageName}`]);
+  }
 
 }
